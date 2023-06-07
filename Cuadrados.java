d8e663a93de81d3a90dbd1fc6944c4b3d032b280
JohnGrubbs07 <133933629+JohnGrubbs07@users.noreply.github.com>
2023-06-07 16:55:08 -0600

    Add files via upload

---
+++ Cuadrados.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana.pkg3.ejercicio.pkg4;
+
+/**
+ *
+ * @author John Paul
+ */
+public class Cuadrados {
+    public static void main(String[] args) {
+        for (int i = 20; i <= 30; i++) {
+            int cuadrado = i * i;
+            System.out.println("Numero: " + i + ", Cuadrado: " + cuadrado);
+        }
+    }
+}
+
+   
